8f814e07f5cb872cae5f9f955dc4327133ff7751
Josef Ziegler <ippeb24@gmail.com>
2013-03-25 18:57:27 +0100

    even more cleaning...

--- Keyboard/QuartzEventServices/compile_quartzeventstuff.sh
+++ Keyboard/QuartzEventServices/compile_quartzeventstuff.sh
@@ -1,11 +1,11 @@
 gcc -Wall -o space  space.c -framework ApplicationServices
 gcc -Wall -o right  right.c -framework ApplicationServices
-gcc -Wall -o left   left.c -framework ApplicationServices
-gcc -Wall -o up     up.c -framework ApplicationServices
-gcc -Wall -o down   down.c -framework ApplicationServices
+gcc -Wall -o left   left.c  -framework ApplicationServices
+gcc -Wall -o up     up.c    -framework ApplicationServices
+gcc -Wall -o down   down.c  -framework ApplicationServices
 gcc -Wall -o shift  shift.c -framework ApplicationServices
-gcc -Wall -o ctrl   ctrl.c -framework ApplicationServices
-gcc -Wall -o x      x.c -framework ApplicationServices
-gcc -Wall -o z      z.c -framework ApplicationServices
-gcc -Wall -o c      c.c -framework ApplicationServices
+gcc -Wall -o ctrl   ctrl.c  -framework ApplicationServices
+gcc -Wall -o x      x.c     -framework ApplicationServices
+gcc -Wall -o z      z.c     -framework ApplicationServices
+gcc -Wall -o c      c.c     -framework ApplicationServices
 
